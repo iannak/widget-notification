ccfadb908c2e202f76a4d3bcac9cee8c37551db8
franzannakarolina <franzannakarolina@pop-os.localdomain>
2023-06-28 09:48:35 -0300

    add themes dark e light

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import Widget from '@/components/Widget'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="h-screen bg-zinc-950 flex items-center justify-center text-zinc-50">
+    <main className="h-screen bg-zinc-50 dark:bg-black flex items-center justify-center text-zinc-800 dark:text-zinc-50">
       <Widget />
     </main>
   )
